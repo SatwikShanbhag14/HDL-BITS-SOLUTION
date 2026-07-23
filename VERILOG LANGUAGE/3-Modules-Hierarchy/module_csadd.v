28adaf0f7989a87ebb38ef705aca64ee2f0d29a4
SatwikShanbhag14 <satwikshanbhag14@gmail.com>
2026-07-23 17:20:21 +0530

    Create module_csadd.v

---
+++ VERILOG LANGUAGE/3-Modules-Hierarchy/module_csadd.v
@@ -0,0 +1,20 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire [15:0] c1,c2;
+    wire [15:0] sum1,sum2;
+    add16 ad1(a[15:0],b[15:0],0,sum[15:0],c1);
+    add16 ad2(a[31:16],b[31:16],0,sum1,c2);
+    add16 ad3(a[31:16],b[31:16],1,sum2,c2);
+    
+    always @(c1,sum1,sum2) begin
+            case (c1)
+                0:sum[31:16]=sum1;
+                1:sum[31:16]=sum2;
+            endcase
+        end
+    
+endmodule
+
